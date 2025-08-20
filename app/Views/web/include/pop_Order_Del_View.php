bcb85f8cc4e6a0df584686c657bf8d1e8e170aae
dawn <mohamin@naver.com>
2025-08-20 11:45:35 +0900

    1145

---
+++ app/Views/web/include/pop_Order_Del_View.php
@@ -0,0 +1,11 @@
+<div class="gunpop orderpopcon" id="endOrderDel" name="endOrderDel">
+    <div class="orderpop">
+        <i class="fa-regular fa-circle-check"></i>
+        <h2>삭제 확인</h2>
+        <p>삭제하시겠습니까?</p>
+        <div>
+            <button class="close_dltmer">취소</button>
+            <button class="go_Order" id="cart_del" data-sn="">확인</button>
+        </div>
+    </div>
+</div>
